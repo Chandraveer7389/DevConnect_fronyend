a716285887326c7227da84546ed0bbf165ac2d1c
Chandraveer <chandraveer7389@gmail.com>
2026-04-15 22:40:44 +0530

    Configured tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-base-200">
+      <h1 className="text-3xl font-bold text-primary">
+        DevTinder Frontend is Ready! 🚀
+      </h1>
+      <button className="mt-4 btn btn-secondary">
+        daisyUI Button Test
+      </button>
+    </div>
+  )
+}
+
+export default App
