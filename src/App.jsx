59a053243e213b401622b88ae1086d14d4cca134
Chandraveer <chandraveer7389@gmail.com>
2026-04-16 00:39:19 +0530

    feat: setup routing & layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from "./Body";
+import Feed from "./Feed";
+import Login from "./Login";
+const appRouter = createBrowserRouter([
+  {
+    path : "/",
+    element : <Body/>,
+    children : [
+      { index: true, element: <Feed /> },
+      { path: "/login", element: <Login /> },
+    ]
+  }
+])
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-base-200">
-      <h1 className="text-3xl font-bold text-primary">
-        DevTinder Frontend is Ready! 🚀
-      </h1>
-      <button className="mt-4 btn btn-secondary">
-        daisyUI Button Test
-      </button>
-    </div>
-  )
+    <RouterProvider router={appRouter} />
+  );
 }
 
-export default App
+export default App;
